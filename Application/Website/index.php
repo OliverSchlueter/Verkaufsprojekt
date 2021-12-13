b82efa32357acd5879b8a2e0b26b27d99b90c46b
OliverSchlueter <oliverhd1303@gmail.com>
2021-12-13 17:49:25 +0100

    [Web] Add needed folders for

--- Application/Website/index.php
+++ Application/Website/index.php
@@ -1,1 +1,13 @@
-hello world
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Verkaufsprojekt</title>
+</head>
+<body>
+
+    <h1>Hello world!</h1>
+    
+</body>
+</html>
